f9b6dfea48d0d13a43a295cc9968bbcbb0226de8
Samuel044547 <148778928+Samuel044547@users.noreply.github.com>
2023-11-06 22:19:25 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,295 @@
+public class Main {
+    public static void main(String[] args) {
+// 1 esep a
+//int n=23;
+//int san1=n/10;
+//int san2=n%10;
+// (san1==3 || san2==3){
+//    System.out.println(" 3 саны кіреді");
+//}else{
+//System.out.println("3 саны кірмейді");
+//}
+// 1 esep b
+//String M="mrjvmjmkdkkkkmma";
+//int a =32;
+//int san1=a/10;
+//int san2=a%10;
+//if (san1==3 || san2==3) {
+//    System.out.println("a san bar ");
+//}else{
+//    System.out.println("a sana jok");
+//}
+//    System.out.println("а әріпі "+M.indexOf("a")+"-ші болып тұр және кіреді" );
+// 2 esep a
+//int N= 14;
+//int san1=N/10;
+//int san2=N%10;
+//if (san1==4 || san2==4){
+//   System.out.println(" 4 саны кіреді");
+//} else if(san1==7||san2==7) {
+//    System.out.println("7 саны кіреді");
+//}else{
+//   System.out.println("4 and 7 саны кірмейді");
+//    }
+// 2 esep b
+//int G=46;
+//int san1=G/10;
+//int san2=G%10;
+//if(san1==3||san2==3){
+//   System.out.println("3 саны кіреді");
+// }else if(san1==6||san2==6) {
+//    System.out.println("6 саны кіреді");
+//}else if (san1==9||san2==9){
+//    System.out.println("9 саны кіреді");
+//}else {
+//   System.out.println("3,6,9 саны кірмейді");
+//}
+// 3 esep a
+//int P=234;
+//int K1=P/100;
+//int K2=P/10%10;
+//int K3=P%10;
+//if (K1==6||K2==6||K3==6){
+//    System.out.println(" 6 саны кіреді");
+//}else {
+//    System.out.println("6 sana jok");
+//}
+// 3 esep b
+//    System.out.println("n әріпі "+H.indexOf("n")+"-ші болып тұр және кіреді");
+//        int n1 =623;
+//        int san12=n1/100;
+//        int san23=n1/10%10;
+//        int san32=n1%10;
+//        if (san12==6 || san23==6|| san32==6) {
+//            System.out.println("n san bar ");
+//        }else {
+//            System.out.println("n sana jok");
+//        }
+// 4 esep a
+//int e=124;
+//int F1=e/100;
+//int F2=e/10%10;
+//int F3=e%10;
+//if (F1==4 || F2==4|| F3==4){
+//    System.out.println(" 4 саны кіреді");
+//} else if(F1==7||F2==7||F3==7) {
+//    System.out.println("7 саны кіреді");
+//}else {
+//    System.out.println("4 and 7 саны кірмейді");
+//}
+// 4 esep b
+//int q=355;
+//int Q1=q/100;
+//int Q2=q/10%10;
+//int Q3=q%10;
+//if(Q1==3||Q2==3||Q3==3){
+//    System.out.println("3 саны кіреді");
+//} else if(Q1==6||Q2==6||Q3==6) {
+//    System.out.println("6 саны кіреді");
+//}else if (Q1==9||Q2==9||Q3==9){
+//    System.out.println("9 саны кіреді");
+//}else {
+//    System.out.println("3,6,9 саны кірмейді");
+//}
+// 5 esep a
+//int A=1415;
+//int N1=A/1000;
+//int N2=A/100%10;
+//int N3=A/10%10;
+//int N4=A%10;
+//if (N1==4 || N2==4||N3==4|| N4==4){
+//    System.out.println("4 kiredi");
+//} else{
+//    System.out.println("4 kirmeidi");
+//}
+// 5 esep b
+//int b=4321;
+// int san16=b/1000;
+// int san15=b/100%10;
+// int san14=b/10%10;
+// int san18=b%10;
+//if (san16==4||san15==4){
+//    System.out.println("b cana kiredi");
+//}else if (san14==4||san18==4) {
+//    System.out.println("b cana kiredi");
+//}else{
+//    System.out.println("b cana kirmeidi");
+//}
+// 6 esep a
+//int B=4266;
+//int M1=B/1000;
+//int M2=B/100%10;
+//int M3=B/10%10;
+//int M4=B%10;
+//if (M1==2||M2==2||M3==2||M4==2){
+//System.out.println("2 kiredi");
+//}else if(M1==7||M2==7||M3==7||M4==7){
+//System.out.println("7 kiredi");
+//}else{
+// System.out.println("2 and 7 kirmeidi");
+//}
+// 6 esep b
+//int Z=4506;
+//int W1=Z/1000;
+//int W2=Z/10%10%10;
+//int W3=Z/10%10;
+//int W4=Z%10;
+//if (W1==3||W3==3||W3==3||W4==3){
+//    System.out.println("3 kiredi");
+//}else if(W1==6||W2==6||W3==6||W4==6){
+//System.out.println("6 kiredi");
+//}else if (W1==9||W2==9||W3==9||W4==9){
+//    System.out.println("9 kiredi");
+//} else {
+//    System.out.println("3,6 and 9 kirmeidi");
+//}
+// 7 esep
+//int n=4547;
+//int san11=n/1000;
+//int san21=n/100%10;
+//int san31=n/10%10;
+//int san41=n %10;
+//if (san11==san31 && san31==san11) {
+//    System.out.println(n + " polidrom san");
+//}else {
+//    System.out.println(n + " polidrom san emes");
+//}
+// 8 esep
+//int k=4447;
+//int San1=k/1000;
+//int San2=k/100%10;
+//int San3=k/10%10;
+//int San4=k %10;
+//if (San1==San2 && San1==San3) {
+//    System.out.println(k + " 3 birdei san bar");
+//}else {
+//    System.out.println(k + " 3 birdei san jok");
+//}
+// 9 esep
+//int p=4689;
+//int SAn1=p/1000;
+//int SAn2=p/100%10;
+//int SAn3=p/10%10;
+//int SAn4=p %10;
+//if (SAn1!=SAn2 && SAn1!=SAn3 && SAn1!=SAn4) {
+//    System.out.println(p + " barlak san birdei emes ");
+//}else {
+//    System.out.println(p + " barlak san birdei ");
+//}
+// 10 esep
+//        int u=123321;
+//int SAN1=u/100000;
+//int SAN2=u/10%10%10;
+//int SAN3=u/100%10;
+//int SAN4=u/1000%10;
+//int SAN5=u/10%10;
+//int SAN6=u %10;
+//int sanu1 = SAN1 + SAN2 + SAN3;
+//int sanu2= SAN4+SAN5+SAN6;
+//if (sanu1==sanu2){
+//    System.out.println(" byl baqatta san");
+//}else{
+//    System.out.println("byl bakatsaz san");
+//}
+// 11 esep
+//int salmak=69;
+//if (salmak<=60){
+//System.out.println("1 jenil salmak");
+//}else if(salmak<=64){
+//System.out.println("2 jartalai-orta salmak");
+//}else if(salmak<=69){
+//    System.out.println("3 jartalai-orta salmak");
+//}else {
+//System.out.println("salmak darezesi jok");
+//}
+// 12 esep
+//int apta=7;
+//if (apta==1){
+//   System.out.println("Monday");
+//} else if(apta==2){
+//    System.out.println("Tuesday");
+//}else if(apta==3){
+//    System.out.println("Wednesday");
+//}else if(apta==3){
+//    System.out.println("Tuesday");
+//}else if(apta==4){
+//    System.out.println("Thursday");
+//}else if(apta==5){
+//    System.out.println("Friday");
+//}else if(apta==6){
+//    System.out.println("Saturday");
+//}else if(apta==7) {
+//            System.out.println("Sunday");
+//}else {
+//    System.out.println("ondai kyn jok");
+//}
+// 13 esep
+//        int ai=12;
+//        if (ai==1){
+//            System.out.println("Desember");
+//        } else if(ai==2){
+//            System.out.println("January");
+//        }else if(ai==3){
+//            System.out.println("February");
+//        }else if(ai==4){
+//            System.out.println("March");
+//        }else if(ai==5){
+//            System.out.println("April");
+//        }else if(ai==6){
+//            System.out.println("May");
+//        }else if(ai==7){
+//            System.out.println("June");
+//        }else if(ai==8) {
+//            System.out.println("Jule");
+//        } else if(ai==9){
+//            System.out.println("August");
+//        }else if(ai==10){
+//            System.out.println("September");
+//        }else if(ai==11){
+//            System.out.println("October");
+//        }else if(ai==12){
+//            System.out.println("November");
+//        } else{
+//                System.out.println("ondai ai jok");
+//        }
+ // 14esep
+        int Desember=1;
+        int January=2;
+        int February=3;
+        int March=4;
+        int April=5;
+        int May=6;
+        int June=7;
+        int Jule=8;
+        int August=9;
+        int September =10;
+        int October=11;
+        int November=12;
+        if (Desember==1){
+            System.out.println("retik nomeri 1");
+        } else if(January==2){
+            System.out.println("retik nomeri 2");
+        }else if(February==3){
+            System.out.println("retik nomeri 3");
+        }else if(March==4){
+            System.out.println("retik nomeri 4");
+        }else if(April==5){
+            System.out.println("retik nomeri 5");
+        }else if(May==6){
+            System.out.println("retik nomeri 6");
+        }else if(June==7){
+            System.out.println("retik nomeri 7");
+        }else if(Jule==8) {
+            System.out.println("retik nomeri 8");
+        } else if(August==9){
+            System.out.println("retik nomeri 9");
+        }else if(September==10){
+            System.out.println("retik nomeri 10");
+        }else if(October==11){
+            System.out.println("retik nomeri 11");
+        }else if(November==12){
+            System.out.println("retik nomeri 12");
+        } else{
+            System.out.println("ondai nomer jol");
+        }
+        }}
